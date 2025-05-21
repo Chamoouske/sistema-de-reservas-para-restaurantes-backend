9346c54845fb3402e22bc81e6ba941b12e546046
Ajax Lima <ajaxlima94@gmail.com>
2025-05-21 20:50:59 -0300

    feat: first commit

---
+++ src/main/java/sistema/de/reserva/de/restaurantes/backend/br/com/SistemaDeReservaDeRestaurantesBackendApplication.java
@@ -0,0 +1,13 @@
+package sistema.de.reserva.de.restaurantes.backend.br.com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaDeReservaDeRestaurantesBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaDeReservaDeRestaurantesBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sistema/de/reserva/de/restaurantes/backend/br/com/SistemaDeReservaDeRestaurantesBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package sistema.de.reserva.de.restaurantes.backend.br.com;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaDeReservaDeRestaurantesBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
